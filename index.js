c3c1b442cb69bceb8a16478277570f29ddc723a2
Steve Peters <sixhops@gmail.com>
2017-06-01 14:57:26 +0000

    Adding pug views and different routes

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-
+var path = require('path');
 var express = require('express');
+var bodyparser = require('body-parser');
 
 var app = express();
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-app.get('*', function(req, res) {
-    res.render('index', {'title': 'pathological', 'message': 'you are the mean clock'});
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public'))); // folder for css and assets
+
+app.get('/green', function(req, res) {
+    res.render('index', {'title': 'pathological', 'message': 'you are the green clock'});
+});
+
+app.get('/blue', function(req, res) {
+    res.render('index', {'title': 'pathological', 'message': 'you are the blue clock'});
+});
+
+app.get('/red', function(req, res) {
+    res.render('index', {'title': 'pathological', 'message': 'you are the red clock'});
+});
+
+app.get('/', function(req, res) {
+    res.render('index', {'title': 'pathological', 'message': 'you are the moon clock'});
 });
 
 app.listen(8080);
